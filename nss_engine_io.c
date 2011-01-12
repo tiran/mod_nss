4aba0ec7bb0902870e50994a8d52427ddf3d3ec6
rcritten <>
2011-01-12 19:43:23 +0000

    Bug 669118
    memcpy of overlapping memory is no longer allowed by glibc.
    
    This is mod_ssl bug https://issues.apache.org/bugzilla/show_bug.cgi?id=45444
    
    Patch ported by Stephen Gallagher.

--- nss_engine_io.c
+++ nss_engine_io.c
@@ -123,13 +123,13 @@ static int char_buffer_read(char_buffer_t *buffer, char *in, int inl)
 
     if (buffer->length > inl) {
         /* we have have enough to fill the caller's buffer */
-        memcpy(in, buffer->value, inl);
+        memmove(in, buffer->value, inl);
         buffer->value += inl;
         buffer->length -= inl;
     }
     else {
         /* swallow remainder of the buffer */
-        memcpy(in, buffer->value, buffer->length);
+        memmove(in, buffer->value, buffer->length);
         inl = buffer->length;
         buffer->value = NULL;
         buffer->length = 0;
