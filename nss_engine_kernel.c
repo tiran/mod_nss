d00a85fbd4700d91316d9b2062acfae01bfd70e8
Matthew Harmsen <mharmsen@redhat.com>
2016-06-28 10:26:36 -0400

    Don't set remote user in fixup hook
    This brings mod_nss inline with mod_ssl.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1347298

--- nss_engine_kernel.c
+++ nss_engine_kernel.c
@@ -953,17 +953,6 @@ int nss_hook_Fixup(request_rec *r)
     }
 
     /*
-     * Set r->user if requested
-     */
-    if (dc->szUserName) {
-        val = nss_var_lookup(r->pool, r->server, r->connection,
-                             r, (char *)dc->szUserName);
-        if (val && val[0]) {
-            r->user = val;
-        }
-    }
-
-    /*
      * Annotate the SSI/CGI environment with standard SSL information
      */
     /* the always present HTTPS (=HTTP over SSL) flag! */
